d43e656dcb53e8c05a463811635d4ed643c6f7f1
ryuwa <ryuusuzuki243@gmail.com>
2023-08-01 19:14:35 +0900

    update: preload時にinvokeをapiとしてセット

--- src/preload.ts
+++ src/preload.ts
@@ -1,2 +1,10 @@
 // See the Electron documentation for details on how to use preload scripts:
 // https://www.electronjs.org/docs/latest/tutorial/process-model#preload-scripts
+import { contextBridge, ipcRenderer } from 'electron';
+contextBridge.exposeInMainWorld(
+  'api', {
+    syncWithZenn: () => ipcRenderer.invoke('sync-with-zenn'),
+    getZennFile: (label: string, file: string) => ipcRenderer.invoke('get-zenn-file', {label, file}),
+    saveZennFile: (label: string, file: string, content: string) => ipcRenderer.invoke('save-zenn-file', {label, file, content})
+  }
+)
