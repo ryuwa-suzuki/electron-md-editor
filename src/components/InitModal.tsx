92d7ae06a1ac94b33c7789c5255ed106e40080a4
ryuwa <ryuusuzuki243@gmail.com>
2023-08-04 11:15:27 +0900

    add: zennディレクトリパス入力モーダルを実装

---
+++ src/components/InitModal.tsx
@@ -0,0 +1,47 @@
+import { Input, Button, Group, Space } from '@mantine/core';
+import { useState } from 'react';
+import { useZennContentContext } from '../App';
+
+interface InitModalProps {
+  closeModal: () => void;
+}
+
+const InitModal: React.FC<InitModalProps> = ({closeModal}) => {
+  const [zennDirPath, setZennDirPath] = useState(localStorage.getItem('zenn_dir_path') || '');
+  const { fetchData } = useZennContentContext();
+  const sync = () => {
+    localStorage.setItem('zenn_dir_path', zennDirPath);
+    fetchData();
+    closeModal();
+  }
+  const  notSync = () => {
+    localStorage.setItem('zenn_dir_path', '');
+    fetchData();
+    closeModal();
+  }
+  return (
+    <>
+      <Input.Wrapper
+        id="input-demo"
+        label="Zennのルートパスを入力"
+      >
+        <Input
+          placeholder="/Users/zenn"
+          value={zennDirPath}
+          onChange={(event) => setZennDirPath(event.target.value)}
+        />
+      </Input.Wrapper>
+      <Space h="xl" />
+      <Group position="center" spacing="xl">
+        <Button onClick={sync}>
+          Zennと同期する
+        </Button>
+        <Button onClick={notSync} variant="outline">
+          同期しないで始める
+        </Button>
+      </Group>
+    </>
+  )
+}
+
+export default InitModal;
