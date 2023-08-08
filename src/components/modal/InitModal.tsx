1d81a8b5df1bdc22db54461bf962a94b48d67cee
ryuwa <ryuusuzuki243@gmail.com>
2023-08-08 19:39:22 +0900

    clean: リファクタリング

--- src/components/modal/InitModal.tsx
+++ src/components/modal/InitModal.tsx
@@ -1,22 +1,23 @@
 import { Input, Button, Group, Space } from '@mantine/core';
-import { useState } from 'react';
-import { useZennContentContext } from '../App';
+// import { useState } from 'react';
+import { useZennContentContext } from '../../contexts/ZennContext';
 
 interface InitModalProps {
   closeModal: () => void;
 }
 
 const InitModal: React.FC<InitModalProps> = ({closeModal}) => {
-  const [zennDirPath, setZennDirPath] = useState(localStorage.getItem('zenn_dir_path') || '');
-  const { fetchData } = useZennContentContext();
-  const sync = () => {
+  const { zennDirPath, syncWithZenn, setZennDirPath, setIsZennSynced } = useZennContentContext();
+  const sync = async () => {
     localStorage.setItem('zenn_dir_path', zennDirPath);
-    fetchData();
+    setZennDirPath(zennDirPath);
+    syncWithZenn();
     closeModal();
   }
   const  notSync = () => {
     localStorage.setItem('zenn_dir_path', '');
-    fetchData();
+    setZennDirPath('');
+    setIsZennSynced(false);
     closeModal();
   }
   return (
