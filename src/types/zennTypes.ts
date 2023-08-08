dfdc25c537cf47d1bfa36b7d4ffd61311ba21a8b
ryuwa <ryuusuzuki243@gmail.com>
2023-08-08 19:42:03 +0900

    clean: リファクタリング

---
+++ src/types/zennTypes.ts
@@ -0,0 +1,29 @@
+type Book = {
+  bookName: string;
+  files: Array<string>;
+};
+export type zennType = {
+  zennData: {
+    content: string;
+    label: string;
+    file: string;
+  };
+  isZennSynced: boolean;
+  fileNames: {
+    articles: Array<string>,
+    books: Array<Book>
+  };
+  zennDirPath: string;
+  selectedFile: {
+    label: string;
+    file: string;
+  };
+  setZennData: (zennData: {content: string, label: string, file: string}) => void;
+  setZennDirPath: (zennDirPath: string) => void;
+  setSelectedFile: (selected: { label: string, file: string }) => void;
+  syncWithZenn: () => Promise<void>;
+  getZennContent: () => Promise<void>;
+  setIsZennSynced: (value: boolean) => void;
+};
+
+
